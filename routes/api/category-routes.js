c41a4ce38afa192fdb40d40a0ecef820e1876188
Jarrete Barnett <jarretebarnett@gmail.com>
2021-04-20 21:50:21 -0400

    Adding code to model folder files.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
-      include: [{ model: Product, as: 'category_product'}]
+      include: [{ model: Product, as: 'category_id'}]
     });
     res.status(200).json(categoryData);
   } catch (err) {
@@ -21,7 +21,7 @@ router.get('/:id', (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-      include: [{ model: Product, as: 'category_product' }]
+      include: [{ model: Product, as: 'category_id' }]
     });
 
     if (!categoryData) {
